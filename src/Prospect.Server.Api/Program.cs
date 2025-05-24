37cff1ff6182378b4c1cdba42e3fa0aef6fdba91
OneMysteryMan <9562024+OneMysteryMan@users.noreply.github.com>
2025-05-24 22:51:31 +1000

    Add file and async console logging

--- src/Prospect.Server.Api/Program.cs
+++ src/Prospect.Server.Api/Program.cs
@@ -1,4 +1,5 @@
 using Serilog;
+using Serilog.Core;
 using Serilog.Events;
 
 namespace Prospect.Server.Api;
@@ -7,13 +8,7 @@ public static class Program
 {
     public static int Main(string[] args)
     {
-        Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Debug()
-            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-            .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
-            .Enrich.FromLogContext()
-            .WriteTo.Console()
-            .CreateLogger();
+        Log.Logger = CreateLogger();
 
         try
         {
@@ -30,6 +25,28 @@ public static class Program
         {
             Log.CloseAndFlush();
         }
+    }
+
+    private static Logger CreateLogger()
+    {
+        const string OutputTemplate = "[{Timestamp:HH:mm:ss.fff zzz} {Level:u3}] {Message:lj} | {Properties:lj} {NewLine}{Exception}";
+
+        return new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database", LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .WriteTo.Async(s => s.Console(outputTemplate: OutputTemplate))
+            .WriteTo.Async(s => s.File(
+                path: "./logs/.txt",
+                outputTemplate: OutputTemplate,
+                rollingInterval: RollingInterval.Day,
+                retainedFileCountLimit: 3,
+                rollOnFileSizeLimit: true,
+                fileSizeLimitBytes: 100 * 1024 * 1024 // 100MB
+            ))
+            .CreateLogger();
     }
 
     private static IHostBuilder CreateHostBuilder(string[] args) =>
